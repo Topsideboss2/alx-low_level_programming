04a9f99cc2286eb8286aaf42245a66c8a0665e1c
Murithi <markkibara2014@gmail.com>
2022-09-10 03:10:35 -0700

    "Add 2-print_alphabet.c"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Execute
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int alf;
+
+	for (alf = 'a'; alf <= 'z'; alf++)
+		putchar(alf);
+		putchar('\n');
+
+	return (0);
+}
