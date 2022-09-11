11b16df8adfa3059b9b523101de0b485fbe16d53
Murithi <markkibara2014@gmail.com>
2022-09-11 13:43:35 -0700

    "Add 8-print_base16.c"

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Execute
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char a, i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
