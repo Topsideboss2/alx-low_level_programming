21075de9766d579f4737dd2a9082dd7cd8a12caf
Murithi <markkibara2014@gmail.com>
2022-09-11 23:33:41 -0700

    "Add 0-putchar.c"

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Execute
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
