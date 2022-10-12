eaa35ad12d0cc4cd34a270bb15413ceeaf62eba3
Murithi <markkibara2014@gmail.com>
2022-10-12 01:30:19 -0700

    "Modified 3-mul.c"

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 
 /**
- * main - multiplues two numbers
- * @argc: number of command line arguments
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  **/
 
 int main(int argc, char *argv[])
+
 {
+
 	if (argc != 3)
 	{
-		print("Error\n");
+		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", (argv[1]) * (argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
+
 }
