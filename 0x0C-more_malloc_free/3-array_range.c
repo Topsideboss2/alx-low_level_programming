049b5166f766d281b07a95bc73305aa893d8a300
Murithi <markkibara2014@gmail.com>
2022-10-14 14:01:46 -0700

    "Modified 3-array_range.c"

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = mallic(sizeof(*ar) * ((max - min) + 1));
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
 	if (ar == NULL)
 		return (NULL);
