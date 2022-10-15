442caae8776de2324a1ee2c7ca300796181fa6fc
Murithi <markkibara2014@gmail.com>
2022-10-15 06:18:50 -0700

    "Modified dog.g"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,10 @@
-#ifndef _DOG_
-#define _DOG_
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - typedef for struct dog
+ **/
+typedef struct dog dog_t;
 
 /**
  * struct dog - structure dog
@@ -14,5 +19,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print-dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
